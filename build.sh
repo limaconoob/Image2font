8b87da4dc8d98d04c62cfa1c94ce4364373cf10c
Jeremy PEPIN <jpepin@e3r10p13.42.fr>
2016-11-24 02:23:51 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 make build run
 docker cp $IMAGE $(make get):/root/image2font/$IMAGE
 docker cp $FONT_INPUT $(make get):/root/image2font/$FONT_INPUT
-docker exec $(make get) python image2font --input-font $FONT_INPUT --output-font $FONT_OUTPUT $IMAGE
+docker exec $(make get) python image2font --input-font $FONT_INPUT --output-font $FONT_OUTPUT --start-index $INDEX $IMAGE
 docker cp $(make get):/root/image2font/$FONT_OUTPUT $FONT_OUTPUT
